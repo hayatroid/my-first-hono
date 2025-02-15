490013de083bd89f1d2bf1afefef9400b10fa58e
hayatroid <hayatroid@gmail.com>
2025-02-15 19:15:35 +0900

    feat: minimal server and client

---
+++ src/client.tsx
@@ -0,0 +1,21 @@
+import { render, use, Suspense } from 'hono/jsx/dom';
+
+const fetchData = async () => {
+  const data = await fetch('/api');
+  return data.json();
+};
+
+const Component = ({ promise }: { promise: Promise<{ message: string }> }) => {
+  const data = use(promise);
+  return <h1>{data.message}</h1>;
+};
+
+const App = () => {
+  return (
+    <Suspense fallback={'loading...'}>
+      <Component promise={fetchData()} />
+    </Suspense>
+  );
+};
+
+render(<App />, document.getElementById('root')!);
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/api', (c) => {
+  return c.json({ message: 'Hello from server' });
+});
+
+app.get('/', (c) => {
+  return c.html(
+    <html>
+      <head>
+        <script type="module" src={import.meta.env.PROD ? '/assets/client.js' : '/src/client.tsx'}></script>
+      </head>
+      <body>
+        <div id="root"></div>
+      </body>
+    </html>
+  );
+});
+
+export default app;
